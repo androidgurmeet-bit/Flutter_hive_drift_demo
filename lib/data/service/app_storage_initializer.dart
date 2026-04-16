eab84628afde90f6a1c2ab3da3a57f6a903dc97a
Gurmeet Singh <gurmeetsingh@IN-J0J26V629N.local>
2026-04-16 19:27:30 +0530

    hive_ce_impl

--- lib/data/service/app_storage_initializer.dart
+++ lib/data/service/app_storage_initializer.dart
@@ -1,4 +1,4 @@
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:product_benchmark_app/data/datasource/product_drift_data_source.dart';
 import 'package:product_benchmark_app/data/datasource/product_hive_data_source.dart';
 
